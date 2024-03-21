2dc011e43df7dd19dcefe3a0635fcfa3f09bd404
Gustavo <134178227+Babidih@users.noreply.github.com>
2024-03-20 22:51:39 -0300

    CreateOperações.py

---
+++ Códigos/Operações.py
@@ -0,0 +1,41 @@
+# Operação de Soma
+X = 100
+Y = 50
+soma = x + y 
+print(soma)
+
+# Operação de Subtração
+X = 100
+Y = 50
+subtracao = x - y 
+print(subtracao)
+
+# Operação de Multiplicação
+X = 100
+Y = 50
+multiplicacao = x * y 
+print(multiplicacao)
+
+# Operação de Divisao com resultado float
+X = 100
+Y = 50
+divisao = x / y 
+print(divisao)
+
+# Operação de Divisao com resultado int
+X = 100
+Y = 50
+divisao = x // y 
+print(divisao)
+
+# Operação de Potenciação
+X = 100
+Y = 50
+potenciacao = x ** y 
+print(potenciacao)
+
+# Operação de Módulo (resto de divisão)
+X = 100
+Y = 50
+modulo = x % y 
+print(modulo)
